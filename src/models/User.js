59f0e66b72cf6bcd147f1cd6a77b3fc9ffbfa65a
Joseph Jerry <josephjeremiah564@yahoo.com>
2021-06-22 13:26:22 +0100

    modified endpoint and model

--- src/models/User.js
+++ src/models/User.js
@@ -1,6 +1,33 @@
 const Validator = require("validator");
 const mongoose = require("mongoose");
 
+// const usersSchema = new mongoose.Schema({
+//   firstName: {
+//     required: true,
+//     type: String,
+//   },
+//   lastName: {
+//     required: true,
+//     type: String,
+//   },
+//   email: {
+//     required: true,
+//     type: String,
+//     unique: true,
+//   },
+//   password: {
+//     required: true,
+//     type: String,
+//   },
+//   phoneNumber: {
+//     required: true,
+//   },
+//   dateJoined: {
+//     type: Date,
+//     default: Date.now,
+//   },
+// });
+
 // SCHEMA
 const usersSchema = new mongoose.Schema({
   // First name
@@ -48,65 +75,53 @@ const usersSchema = new mongoose.Schema({
       },
       "Please provide your email.",
     ],
-    unique: [
-      true,
-      `Your email must be unique, there's already a user with '{VALLUE}'.`,
-    ],
+    unique: [true, `User Already Exists'.`],
     lowercase: true,
     validate: [Validator.isEmail, "Please provide a valid email."],
   },
   // Phone number
   phoneNumber: {
     type: String,
-    required: [
-      function () {
-        return this.email;
-      },
-      "Please provide your phone number.",
-    ],
-    unique: [
-      true,
-      `Your phone number must be unique,there's already a user with '{VALLUE}'.`,
-    ],
-    validate: [Validator.isMobilePhone, "Please provide a valid phone number."],
+    required: true,
+    // unique: [true, `Phone Number Already exists..`],
   },
   // Password
   password: {
     type: String,
-    required: [
-      function () {
-        return this.phoneNumber;
-      },
-      "Please proide your password.",
-    ],
+    required: true,
     minlength: [8, `Your password should be atleast 8 characters long.`],
-    select: false,
   },
-  // Confirm password
-  passwordConfirm: {
-    type: String,
-    required: [
-      function () {
-        return this.password;
-      },
-      "Please confirm your password.",
-    ],
-    validate: {
-      validator: function (el) {
-        return el === this.password;
-      },
-      message: "Passwords are not the same!",
-    },
+  dateJoined: {
+    type: Date,
+    default: Date.now,
   },
+  // // Confirm password
+  // passwordConfirm: {
+  //   type: String,
+  //   required: [
+  //     function () {
+  //       return this.password;
+  //     },
+  //     "Please confirm your password.",
+  //   ],
+  //   validate: {
+  //     validator: function (el) {
+  //       return el === this.password;
+  //     },
+  //     message: "Passwords are not the same!",
+  //   },
+  // },
   // Role
-  role: {
-    type: String,
-    enum: ["user", "admin"],
-    default: "user",
-  },
+  // role: {
+  //   type: String,
+  //   enum: ["user", "admin"],
+  //   default: "user",
+  // },
 });
 // password hash
+
 // compare passwords instance
+
 // MODEL
 const User = mongoose.model("User", usersSchema);
 module.exports = User;
